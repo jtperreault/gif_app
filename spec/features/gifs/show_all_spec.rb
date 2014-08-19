be8d4cd43179b7c5422b82a0715584f162c705de
James T. Perreault <james@perreau.lt>
2014-08-19 12:48:21 -0400

    spec for showing all gifs

---
+++ spec/features/gifs/show_all_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature "User views" do
+  let!(:gif1) { create(:gif, title: 'Gif One') }
+  let!(:gif2) { create(:gif, title: 'Gif Two') }
+  let!(:gif3) { create(:gif, title: 'Gif Three') }
+  before { visit all_gifs_path }
+
+  scenario "all gifs" do
+    expect(page).to have_content('All Gifs')
+    expect(page).to have_content(gif1.title)
+    expect(page).to have_content(gif2.title)
+    expect(page).to have_content(gif3.title)
+  end
+end
