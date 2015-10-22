3db42e5aecb39d6c9d0edb0f2a5a18d47a835df5
James T. Perreault <james@perreau.lt>
2015-10-21 22:36:33 -0400

    Adds capistrano for deployments Upgrade to Ruby 2.2.3

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.1.6'
+gem 'rails', '4.2.4'
 gem 'pg'
 gem 'uglifier', '>= 1.3.0'
 gem 'sass-rails', '~> 4.0.3'
@@ -9,18 +9,28 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 
+# View
 gem 'slim-rails'
 gem 'bourbon'
 gem 'neat'
+gem 'selectize-rails'
+
+# Auth
 gem 'devise'
 gem 'acts-as-taggable-on'
 gem 'figaro'
+
+gem 'responders', '~> 2.0'
+
+# Server
+gem 'puma'
 
 group :development do
   gem 'spring'
   gem 'capistrano-rails'
   gem 'capistrano-chruby'
   gem 'capistrano-bundler'
+  gem 'web-console', '~> 2.0'
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,64 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'gif_app'
+set :repo_url, 'https://github.com/jtperreault/gif_app.git'
+set :chruby_ruby, ENV["CAP_CHRUBY"]
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, ENV["CAP_DEPLOY_TO"]
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/application.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  task :upload_config do
+    on roles(:app) do
+      upload!('config/application.yml', "#{shared_path}/config/application.yml")
+    end
+  end
+
+  task :symlink_config do
+    on roles(:app) do
+      execute "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
