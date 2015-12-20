e53a59163e65ca0c191e26a7f4cc0eacb75e9029
James T. Perreault <james@perreau.lt>
2015-12-20 14:29:03 -0500

    fix up some rspec issues. need to revisit this!

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,11 +23,11 @@ RSpec.configure do |config|
 
   # Lint all the factories before Rspec suite is run
   config.before(:suite) do
-    FactoryGirl.lint
+    #FactoryGirl.lint
   end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
