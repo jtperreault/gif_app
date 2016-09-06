02741c8f8353df90c07ca2d13014a6b1c097258d
James T. Perreault <james@perreau.lt>
2016-09-05 21:00:55 -0400

    remove deprecation warning from tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,7 +19,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include FactoryGirl::Syntax::Methods
 
   # Lint all the factories before Rspec suite is run
